279e1140affee275d7f8967c611826d55267d4d9
MusatovDA <damusatov@mail.ru>
2023-04-26 22:54:40 +0300

    task_4

--- HomeWorks/HomeWork1/Program.cs
+++ HomeWorks/HomeWork1/Program.cs
@@ -4,7 +4,6 @@
 // int num1 = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Input second number:");
 // int num2 = Convert.ToInt32(Console.ReadLine());
-
 // if (num1 > num2)
 // {
 //     Console.WriteLine($"{num1} -> max namber, {num2} -> min namber");
@@ -18,12 +17,16 @@
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
     
 // Console.WriteLine("Enter three numbers:");    
+
 // Console.Write("Input first number: ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
+
 // Console.Write("Input second number: ");
 // int num2 = Convert.ToInt32(Console.ReadLine());
+
 // Console.Write("Input third number: ");
 // int num3 = Convert.ToInt32(Console.ReadLine());
+
 // int max = Math.Max(num1, Math.Max(num2, num3));
 // if (max == num1)
 // {
@@ -47,4 +50,18 @@
 //     Console.WriteLine($" число {num} является четным");
 // else 
 //     Console.WriteLine($" число {num} является не четным");
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Enter number:");
+// int Num = Convert.ToInt32(Console.ReadLine());
+// int evenNum =2;
+// if (Num > 1)
+// {
+//     while(evenNum <= Num)
+//     {
+//         Console.Write(evenNum + " ");
+//         evenNum = evenNum + 2;
+//     }
+// }
 
