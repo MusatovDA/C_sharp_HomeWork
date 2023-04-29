28fd180f53946f7e0ff45178285bd20aca97a72c
MusatovDA <damusatov@mail.ru>
2023-04-29 11:38:22 +0300

    task_13

--- HomeWorks/HomeWork2/Program.cs
+++ HomeWorks/HomeWork2/Program.cs
@@ -9,3 +9,33 @@
 // }
 // else
 //     Console.WriteLine("Это число не является трехзначным!");
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Prompt(string msg)
+{
+Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int num = Prompt("Введите число: ");
+
+int fnum(int num)
+{
+while (num > 999)
+{
+num /= 10;
+}
+return num % 10;
+}
+
+bool check(int number)
+{
+if (num < 100)
+return false;
+else return true;
+}
+
+if (check(num) != true)
+System.Console.WriteLine("В введеном числе третьей цифры нет!");
+else
+System.Console.WriteLine($"Третьей цифрой числа {num} является {fnum(num)}");
